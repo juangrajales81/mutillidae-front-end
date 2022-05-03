4172eaf7e2c6693fa2972713aeae73a4fc9ad4bf
juangrajales81 <juangrajales81@gmail.com>
2022-05-03 09:36:01 -0500

    Homepage thumbnails

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,7 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  imgPath: string = 'assets/images/';
+  thumbnails : any = {
+    gettingStarted : this.imgPath + 'getting-started_250x250.jpg',
+    whatShouldIDo : this.imgPath + 'what-should-i-do_250x250.jpg',
+    helpMe : this.imgPath + 'help-me_250x250.jpg',
+    videoTutorials : this.imgPath + 'video-tutorials_250x250.jpg',
+    listingOfVulnerabilities : this.imgPath + 'listing-of-vulnerabilities_250x250.jpg',
+    helpfulHintsAndScripts : this.imgPath + 'helpful-hints-and-scripts_250x250.jpg',
+    donate : this.imgPath + 'donate_250x250.jpg',
+    mutilliadeLdifFile : this.imgPath + 'mutilliade-ldif-file_250x250.jpg',
+  };
+
+  constructor() {
+
+
+
+  }
 
   ngOnInit(): void {
   }
